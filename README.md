0df5040f051e4ac915d5577f38f45717ff943bd3
CynthiaKarinaa <57882859+CynthiaKarinaa@users.noreply.github.com>
2021-02-03 00:44:03 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DIG4639-ex2
+
+"Whatever will be, will be" -Doris Day
---
+++ index.js
@@ -0,0 +1,9 @@
+let quotes = [
+   ` "Whatever will be, will be" 
+    -Doris Day`,
+    ` "I hope the leaving is joyful; and I hope never to return."
+    -Frida Kahlo`,
+]
+
+let index = Math.floor(Math.random() * 2)
+console.log(quotes[0])
