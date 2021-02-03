17c6360954e03d8a73a45d9844d1adfb12246004
CynthiaKarinaa <57882859+CynthiaKarinaa@users.noreply.github.com>
2021-02-03 00:53:42 -0500

    Closes #1

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let quotes = [
     -Doris Day`,
     ` "I hope the leaving is joyful; and I hope never to return."
     -Frida Kahlo`,
+    `"Be the change you want to see in the world.
+    -Mahatma Gandi`,
 ]
 
-let index = Math.floor(Math.random() * 2)
+let index = Math.floor(Math.random() * 3)
 console.log(quotes[0])
